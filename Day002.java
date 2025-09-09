903e959c31945ab723dab6e9df866641c3e2a218
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2025-09-09 15:13:03 +0800

    Day2 : println, printf, print

---
+++ Day002.java
@@ -0,0 +1,11 @@
+package Coding2025;
+
+public class Day2 {
+
+    public static void main(String[] args) {
+        System.out.println("ini adalah System.out.println");
+        System.out.print("ini adalah System.out.print");
+        System.out.printf("\nini adalah System.out.printf\n");
+    }
+
+}
