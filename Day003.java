552cbb9b10f5522e1a8fb3afaebddb2f0cbeeb01
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2025-09-12 17:50:23 +0800

    Day3_Komentar

---
+++ Day003.java
@@ -0,0 +1,11 @@
+package Coding2025;
+
+public class Day3 {
+
+    public static void main(String[] args) {
+        //Day 3 Komentar
+        //Ini adalah komentar satu baris
+        /*Ini adalah komentar untuk lebih dari satu baris*/
+    }
+
+}
