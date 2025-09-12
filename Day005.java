ee12107feb7f3f83fbbf3e23186d041d07552bff
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2025-09-12 17:59:20 +0800

    TipeDataNumerik.java
    Tipe Data Numerik Bilangan Bulat (byte, short, int, long).

---
+++ Day005.java
@@ -0,0 +1,17 @@
+package Coding2025;
+
+public class Day5 {
+
+    //Tipe Data Numerik Bilangan Bulat (byte, short, int, long).
+    public static void main(String[] args) {
+        byte tanggal = 12;
+        short bulan = 9;
+        int tahun = 2025;
+        long tipeLong = 900000000;
+
+        System.out.println("Tanggal = " + tanggal);
+        System.out.println("Bulan = " + bulan);
+        System.out.println("Tahun = " + tahun);
+        System.out.println("ini adalah tipe data long = " + tipeLong);
+    }
+}
