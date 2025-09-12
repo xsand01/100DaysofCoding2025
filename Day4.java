a55bebb9dbbf16b57460414f9ebc3fdb6a62e8ab
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2025-09-12 17:51:20 +0800

    Day4_EscapeSequance

---
+++ Day4.java
@@ -0,0 +1,16 @@
+package Coding2025;
+
+public class Day4 {
+    // Day 4 Escape Squence
+
+    public static void main(String[] args) {
+        String nama = "Kurnia \nSandy";
+        String umur = "18";
+        String jur = "Informatika";
+        String s = "Hilangkan Huruf \bB";
+        System.out.println(nama);
+        System.out.println(umur);
+        System.out.println(jur);
+        System.out.println(s);
+    }
+}
