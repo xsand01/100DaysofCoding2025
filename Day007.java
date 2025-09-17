696a0e7221485377f78ddf47cab81d0e9b1fa723
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2025-09-17 11:10:39 +0800

    Tipe Data Karakter (char) dan Boolean (boolean).

--- Day007.java
+++ Day007.java
@@ -1,1 +1,20 @@
+package Coding2025;
+
+import java.util.Scanner;
+
+public class Day7 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Masukan Huuf Vokal = ");
+        char huruf = in.next().charAt(0);
+        
+        char h = Character.toLowerCase(huruf);
+        boolean cek = (h == 'a' || h == 'i' || h == 'u' || h == 'e' || h == 'o');
+        
+        System.out.println("Apakah huruf " + huruf + " adalah huruf vokal ?");
+        System.out.println("jawaban = " + cek);
+
+    }
+}
 
