942862e20e731333a840d440fad076debc598b3b
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2025-09-17 11:26:04 +0800

    Deklarasi, Inisialisasi dan Update Variabel.

---
+++ Day009.java
@@ -0,0 +1,16 @@
+package Coding2025;
+
+public class Day9 {
+
+    public static void main(String[] args) {
+
+        int tabungan = 1000_000;
+        System.out.println("Tabungan sebelum di update = Rp" + tabungan);
+        tabungan += 500_000; //Gaji masuk
+        System.out.println("Tabungan setelah gaji masuk = Rp" + tabungan);
+        tabungan -= 200_000; // untuk belanja
+        System.out.println("Tabungan setelah di pakai belanja = Rp" + tabungan);
+
+    }
+
+}
