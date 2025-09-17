c671b2214a430719a860ae89ece6727e0884aaab
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2025-09-17 11:18:36 +0800

    Pengenalan Tipe Data String untuk teks

---
+++ Day008.java
@@ -0,0 +1,16 @@
+package Coding2025;
+
+import java.util.Scanner;
+
+public class Day8 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Masukan Nama Lengkap = ");
+        String nama = in.nextLine();
+        System.out.print("Masukan Jurusan = ");
+        String jurusan = in.nextLine();
+        System.out.println("Haiii, nama saya " + nama + " Jurusan saya " + jurusan);
+    }
+
+}
