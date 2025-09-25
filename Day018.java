18aa1a6d5845d4642883ceb0e4d1cb3b189db1fe
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2025-09-25 12:08:35 +0800

    Konversi_Otomatis

---
+++ Day018.java
@@ -0,0 +1,25 @@
+package Coding2025;
+
+public class Day18 {
+
+    public static void main(String[] args) {
+        //  
+        byte a = 10;
+        short b = a;
+        int c = b;
+        long d = c;
+        System.out.println(d);
+
+        float e = 12.2f;
+        double f = e;
+
+        System.out.println(f);
+        
+        int r = 10;
+        double z = r;
+        
+        System.out.println(z);
+        
+
+    }
+}
