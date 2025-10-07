78727d4f6ae9173ea33bf452c3fb2fbd5a95489d
Kurnia_Sandy01 <kurniasandy201m@gmail.com>
2025-10-07 09:01:54 +0800

    Operator Perbandingan == dan !=

---
+++ Day028.java
@@ -0,0 +1,19 @@
+package Coding2025;
+
+import java.util.Scanner;
+
+public class Day28 {
+
+    public static void main(String[] args) {
+        //Operator Perbandingan == dan !=
+        Scanner in = new Scanner(System.in);
+        System.out.println("=== Program Operator Perbandingan (== dan !=) ===");
+        System.out.print("Masukan angka pertama = ");
+        int a = in.nextInt();
+        System.out.print("Masukan angka kedua = ");
+        int b = in.nextInt();
+        System.out.println("Apakah angka pertama == angka ke dua? = " + (a == b));
+        System.out.println("Apakah angka pertama != angka ke dua? = " + (a != b));
+
+    }
+}
